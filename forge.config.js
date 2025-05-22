a223be493a37ac5e7e451c45458dc3bf62f79ac7
Joel Wickard <jwickard@gmail.com>
2025-05-21 23:59:32 -0500

    feat: add Ubuntu DEB package configuration

--- forge.config.js
+++ forge.config.js
@@ -14,7 +14,20 @@ module.exports = {
     },
     {
       name: '@electron-forge/maker-deb',
-      config: {},
+      config: {
+        options: {
+          maintainer: 'Joel Wickard',
+          homepage: 'https://github.com/joelwickard/moog-muse-manager',
+          categories: ['Audio', 'Music'],
+          description: 'A desktop application to manage sound patches and step-sequencer files for the Moog Muse synthesizer',
+          icon: './assets/icon.png',
+          mimeType: ['audio/midi'],
+          bin: 'moog-muse-manager',
+          section: 'sound',
+          priority: 'optional',
+          depends: ['libgtk-3-0', 'libnotify4', 'libnss3', 'libxss1', 'libxtst6', 'xdg-utils', 'libatspi2.0-0', 'libuuid1', 'libsecret-1-0', 'libasound2']
+        }
+      }
     },
     {
       name: '@electron-forge/maker-rpm',

2a65758095b49485f5839dcaec95abcaf392fba6
Joel Wickard <jwickard@gmail.com>
2025-05-21 22:01:57 -0500

    refactor: move config files to dedicated config directory

---
+++ config/jest.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1'
+  },
+  setupFiles: ['<rootDir>/config/jest.setup.js'],
+  setupFilesAfterEnv: [
+    '<rootDir>/src/renderer/__tests__/jest.setup.renderer.js'
+  ],
+  testMatch: ['**/__tests__/**/*.test.[jt]s?(x)'],
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest'
+  },
+  transformIgnorePatterns: [
+    'node_modules/(?!(better-sqlite3)/)'
+  ]
+};

b89d4d073fc26ae66d70f14bc0a96cf841e6c61f
Joel Wickard <jwickard@gmail.com>
2025-05-21 22:39:15 -0500

    fix: update Jest test path configuration

--- config/jest.main.config.js
+++ config/jest.main.config.js
@@ -1,7 +1,8 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  testMatch: ['<rootDir>/../src/main/__tests__/**/*.test.ts'],
+  rootDir: '..',
+  testMatch: ['src/main/__tests__/**/*.test.ts'],
   transformIgnorePatterns: [
     'node_modules/(?!(better-sqlite3)/)'
   ]
